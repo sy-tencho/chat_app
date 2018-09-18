64551775c73f829c37b479159b27f2d45fc02e74
Shota Yamazaki <sysy5867@gmail.com>
2018-09-18 16:22:16 +0900

    modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180918020941) do
+ActiveRecord::Schema.define(version: 20180918032702) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180918020941) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
