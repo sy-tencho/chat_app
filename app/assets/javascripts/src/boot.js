f9d75a3326a91de09051d0c14df3b55d31cb8fec
Shota Yamazaki <sysy5867@gmail.com>
2018-09-21 18:40:25 +0900

    boot.js

--- app/assets/javascripts/src/boot.js
+++ app/assets/javascripts/src/boot.js
@@ -1,11 +1,23 @@
 import 'babel-polyfill'
 import $ from './vendor/jquery'
 import page from 'page'
+import MessageRouter from './router/message'
 import UserRouter from './router/user'
 
 $(() => {
+  const messageRouter = new MessageRouter()
+  messageRouter.register()
+
   const userRouter = new UserRouter()
   userRouter.register()
 
   page({click: false})
 })
+
+// $(() => {
+//   const userRouter = new UserRouter()
+//   userRouter.register()
+
+//   page({click: false})
+// })
+// $(() => {})を２つ作ると失敗する
